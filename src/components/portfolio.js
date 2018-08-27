22c570e6e3b59c778f91783266187649d5ed5a1e
ajholsinger <ajholsinger.inc@gmail.com>
2018-08-27 11:07:40 -0400

    Second test commit

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = () => (
     <div className="row jumboRow">
       <div className="opening">
         <div className="projects">
-          <h3 className="projecth3">Major Projects</h3>
+          <h3 className="projecth3">Major Projects AUSTIN HOLSINGER</h3>
           <div className="port1">
             <h1>Project</h1>
           </div>
